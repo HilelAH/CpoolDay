3d6cf0feb56fe11ab8b4b66dc5c6ab8fa757da32
Hilel <hilel.alvarez-huguet@epitech.eu>
2025-07-05 15:35:44 +0200

    dixseptièmeBite

--- my_print_square.c
+++ my_print_square.c
@@ -9,9 +9,18 @@ void my_print_square(int size, char c)
         return;
     }
     for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
+        if (i == 0 || i == size) {
+            for (int j = 0; j < size; j++) {
+                print_char(c);
+            }
+        } else {
+            print_char(c);
+            for (int j = 0; j < size - 2; j++) {
+                print_char(' ');
+            }
             print_char(c);
         }
+        
         print_char('\n');
     }
 }
