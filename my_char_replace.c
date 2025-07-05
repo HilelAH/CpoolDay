4b7709239e5402a11b29e2e3002fd942cdd385c1
Hilel <hilel.alvarez-huguet@epitech.eu>
2025-07-05 11:41:40 +0200

    bite

---
+++ my_char_replace.c
@@ -0,0 +1,12 @@
+int my_len(char *c);
+
+char *my_char_replace(char *origin, char toFind, char toReplace)
+{
+    char *tmp = origin;
+    for (int i = 0; i < my_len(origin); i++) {
+        if (origin[i] == toFind) {
+            tmp[i] = toReplace;
+        }
+    }
+    return tmp;
+}
