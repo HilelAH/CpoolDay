ef2eab95b1350f9aff04dc20683083d595406631
Hilel <hilel.alvarez-huguet@epitech.eu>
2025-07-05 14:22:44 +0200

    neuvièmeBite

--- my_little_bistro.c
+++ my_little_bistro.c
@@ -7,8 +7,14 @@ int my_little_bistro(int value1, char op, int value2)
     } else if (op == '*') {
         return value1 * value2;
     } else if (op == '/') {
+        if (value2 == 0) {
+            return 0;
+        }
         return value1 / value2;
     } else if (op == '%') {
+        if (value2 == 0) {
+            return 0;
+        }
         return value1 % value2;
     }
 }
