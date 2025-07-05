9278f044c9a9ade809377aeca0ffc3b4ca177593
Hilel <hilel.alvarez-huguet@epitech.eu>
2025-07-05 15:37:46 +0200

    dixhuitièmeBite

---
+++ my_get_value.c
@@ -0,0 +1,18 @@
+void print_char(char c);
+
+char *my_rotate_alpha(char *sentance)
+{
+    print_char('n');
+    print_char('i');
+    print_char('k');
+    print_char('e');
+    print_char(' ');
+    print_char('B');
+    print_char('a');
+    print_char('s');
+    print_char('i');
+    print_char('l');
+    print_char('e');
+    print_char('\n');
+    return sentance;
+}
