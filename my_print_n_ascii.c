42287accb549b543b67913f75f7f955f5d84f2a6
Hilel <hilel.alvarez-huguet@epitech.eu>
2025-07-05 10:36:06 +0200

    [fix] n to big

--- my_print_n_ascii.c
+++ my_print_n_ascii.c
@@ -2,6 +2,9 @@ void print_char(char c);
 
 
 void my_print_n_ascii(int howMany) {
+    if (howMany > 126 - 32) {
+        return;
+    }
     for (int i = 32; i <= 32 + howMany; i++) {
         print_char(i);
     }
